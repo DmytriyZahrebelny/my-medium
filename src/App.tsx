a64bf23b860c5afb81bbe6882cbd3564791f184c
Dima <zahrebelny@gmail.com>
2020-01-23 23:24:03 +0200

    ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
-const App: React.FC = () => {
-	return (
-		<div className='App' id='sdsdfs'>
-			<header className='App-header'>
-				<p>Edit</p>
-				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-					Learn React
-				</a>
-				<p className='sdfsf'>sdf</p>
-			</header>
-		</div>
-	);
-};
+const App: React.FC = () => (
+	<div className='App' id='sdsdfs'>
+		<header className='App-header'>
+			<p>Edit</p>
+			<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+				Learn React
+			</a>
+			<p className='sdfsf'>sdf</p>
+		</header>
+	</div>
+);
 
 export default App;

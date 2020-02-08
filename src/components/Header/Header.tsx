dd6763f21b10e1521429aa0fcd549591a43322ac
Dima <zahrebelny@gmail.com>
2020-02-08 02:21:15 +0200

    added header with auth state

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Icon from '../components/Icon';
+import { RootState } from '../../store/configureStore';
 import './header.sass';
 
+const userLogo: string = 'https://cdn-images-1.medium.com/fit/c/32/32/0*7TqUrB-WnHVdrBF5.';
+
 const Header: React.FC = () => {
+	const user = useSelector((state: RootState) => state.authStore.user);
 	return (
 		<header className='header'>
 			<div className='header__container'>
 				<Link className='header__logo' to='/'>
-					Logo
+					<Icon name='logo' size={114} />
 				</Link>
 				<div className='header__menu'>
-					<Link className='header__link' to='/signin'>
-						Sign In
-					</Link>
-					<Link className='header__link' to='/signup'>
-						Sign Up
-					</Link>
+					{user ? (
+						<span className='header__user'>
+							<img className='header__icon' src={user.image || userLogo} alt='user' />
+						</span>
+					) : (
+						<>
+							<Link className='header__link' to='/signin'>
+								Sign In
+							</Link>
+							<Link className='header__link' to='/signup'>
+								Sign Up
+							</Link>
+						</>
+					)}
 				</div>
 			</div>
 		</header>
